39aab8fca0a08e9073ef87f7a1acb678b8af101d
jun094 <jun094@naver.com>
2023-04-04 01:11:15 +0900

    feat: AiPage 2차 퍼블리싱

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,13 +1,14 @@
 import styles from './Textarea.module.scss';
 
 type TextareaProps = {
-  placeholder?: 'Try writing in English here, or paste an existing one';
-  value?: string;
-  onChange: any;
-};
+  placeholder?: string;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-const Textarea = ({ placeholder, value, onChange }: TextareaProps) => {
-  return <textarea className={styles.textarea} placeholder={placeholder} value={value} onChange={onChange} />;
+const Textarea = ({
+  placeholder = 'Try writing in English here, or paste an existing one',
+  ...rest
+}: TextareaProps) => {
+  return <textarea className={styles.textarea} placeholder={placeholder} {...rest}></textarea>;
 };
 
 export default Textarea;
--- src/pages/AiPage/AiPage.tsx
+++ src/pages/AiPage/AiPage.tsx
@@ -5,9 +5,12 @@ import PageWrapper from 'components/PageWrapper';
 import Textarea from 'components/Textarea';
 
 import styles from './AiPage.module.scss';
+import Button from 'components/Button';
+import Select from 'components/Select';
 
 function AiPage() {
   const [value, setValue] = useState<string | undefined>(undefined);
+  const [disabled, setDisabled] = useState<boolean>(true);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
@@ -18,13 +21,22 @@ function AiPage() {
     <>
       <Navigation />
       <PageWrapper>
-        <section className={styles.titleSection}>
-          <h1 className={styles.title}>Let&apos;s write English</h1>
-          <p className={styles.subTitle}>AI tutor to improve your English writing skills</p>
+        <section className={styles.header}>
+          <h1 className={styles.title}>Let&apos;s get your writing revised!</h1>
         </section>
 
-        <section className={styles.inputSection}>
+        <section className={styles.body}>
+          <div className={styles.selects}>
+            <Select label="purpose" list={['list1', 'list2', 'list3']} className={styles.select} />
+            <Select label="style" list={['list1', 'list2', 'list3']} className={styles.select} />
+            <Select label="tone" list={['list1', 'list2', 'list3']} className={styles.select} />
+          </div>
+
           <Textarea value={value} onChange={handleChange} />
+        </section>
+
+        <section className={styles.footer}>
+          <Button disabled={disabled}>SUBMIT</Button>
         </section>
       </PageWrapper>
     </>
