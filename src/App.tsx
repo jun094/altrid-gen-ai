245696563d75c3898ecdc9ced03566990fd25133
jun094 <jun094@naver.com>
2023-04-06 00:34:13 +0900

    feat: clipboard 복사 기능 및 react-toastify 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ToastContainer } from 'react-toastify';
 import PageRoutes from 'pages/PageRoutes';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return <PageRoutes />;
+  return (
+    <>
+      <ToastContainer autoClose={1500} hideProgressBar={true} />
+      <PageRoutes />
+    </>
+  );
 }
 
 export default App;
--- src/pages/CoachPage/CoachPage.tsx
+++ src/pages/CoachPage/CoachPage.tsx
@@ -1,3 +1,5 @@
+import { toast } from 'react-toastify';
+
 import Navigation from 'components/Navigation';
 import PageWrapper from 'components/PageWrapper';
 
@@ -10,10 +12,20 @@ import Button from 'components/Button';
 import { ReactComponent as CopyIcon } from 'styles/assets/clipboard.svg';
 import { ReactComponent as SendIcon } from 'styles/assets/send-white.svg';
 
+import useCopyToClipboard from 'hooks/useCopyToClipboard';
+
 const DUMMY_SUBMITTED =
   'Topic: Have you ever seen someone being bullied? What can you do to help them? When it comes to decide whether we stop bullies from hurting someone, it can be very complicated to act. Some people think freedom means that they can do anything they want, however rules are to not laugh or tease at people because of thier behaviors and looks. I strongly belive that freedom cannot allow people to do whatever they want to do if they hurt other people. I will personally help them if I see someone being bullied. First, freedom must not allow people to do whatever they want to. The morality is to respect, be heart-warming, and to care about people. When it comes to the situation when we need to decide the importance of freedom or morality over another, we have to be clear about what we need to choose and to become. For example, if there are too much freedom in the world, it will become enourmous disaster where people will steal valuable things, trash things everywhere, and eat others’ food.';
+const DUMMY_EDITED =
+  '**** EDITED **** Topic: Have you ever seen someone being bullied? What can you do to help them? When it comes to decide whether we stop bullies from hurting someone, it can be very complicated to act. Some people think freedom means that they can do anything they want, however rules are to not laugh or tease at people because of thier behaviors and looks. I strongly belive that freedom cannot allow people to do whatever they want to do if they hurt other people. I will personally help them if I see someone being bullied. First, freedom must not allow people to do whatever they want to. The morality is to respect, be heart-warming, and to care about people. When it comes to the situation when we need to decide the importance of freedom or morality over another, we have to be clear about what we need to choose and to become. For example, if there are too much freedom in the world, it will become enourmous disaster where people will steal valuable things, trash things everywhere, and eat others’ food.';
 
 function CoachPage() {
+  const [_, copy] = useCopyToClipboard();
+
+  const handleCopy = () => {
+    copy(DUMMY_EDITED);
+    toast('copied!');
+  };
   return (
     <>
       <Navigation />
@@ -38,14 +50,14 @@ function CoachPage() {
 
           {/* 편집 가능한 textarea */}
           <div className={styles.textarea}>
-            <div className={styles.editingHeader}>
-              <h6 className={styles.editingTitle}>AI writing coach</h6>
+            <div className={styles.editedHeader}>
+              <h6 className={styles.editedTitle}>AI writing coach</h6>
 
-              <Button size="md" style="outline" icon={CopyIcon}>
+              <Button size="md" style="outline" icon={CopyIcon} onClick={handleCopy}>
                 Copy
               </Button>
             </div>
-            <Textarea value={DUMMY_SUBMITTED} minHeight={540} readOnly />
+            <Textarea value={DUMMY_EDITED} minHeight={540} readOnly />
           </div>
         </section>
 
