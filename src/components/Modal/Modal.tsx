19c4ff86385c0ed90a8e465208d96cbb650fded8
jun094 <jun094@naver.com>
2023-04-08 18:13:46 +0900

    feat: Modal prop 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,18 +1,20 @@
+import cn from 'classnames';
 import { AppPortal } from 'AppPortal';
 import styles from './Modal.module.scss';
 
 type ModalProps = {
   isShow?: boolean;
+  className?: string;
   children: React.ReactNode;
 };
 
-const Modal = ({ isShow = false, children }: ModalProps) => {
+const Modal = ({ isShow = false, className, children }: ModalProps) => {
   return (
     <>
       {isShow && (
         <AppPortal.Consumer>
           <div className={styles.container}>
-            <div className={styles.modal}>{children}</div>
+            <div className={cn(styles.modal, className)}>{children}</div>
           </div>
         </AppPortal.Consumer>
       )}
